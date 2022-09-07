442a09fdfc8b05218d04664c69c46f5276163c25
Guilherme Lopes Ferreira <gferreira525@gmail.com>
2022-09-07 19:21:29 -0300

    nft sample contract

--- contracts/LiquidArt721.sol
+++ contracts/LiquidArt721.sol
@@ -1,12 +1,24 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: ISC
 pragma solidity ^0.8.9;
 
 import "@oppenzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract LiquidArt721 is ERC721 {
+    uint public nextTokenId;
+    address public admin;
 
- constructor() ERC721("Liquid Art", "LQART") {
-    
+ constructor() ERC721('Liquid Art', 'LQART') {
+    admin = msg.sender;
+ }
+
+ function mint(address to) external {
+    require(msg.sender == admin, 'only admin');
+    _safeMint(to, nextTokenId);
+    nextTokenId++;
+ }
+
+ function _baseURI() internal view override returns (string memory){
+    return 'http://localhost:1313';
  }
 
 }
